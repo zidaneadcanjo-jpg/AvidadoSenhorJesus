9970c0033b1b33cc179766d6d444582615906cfd
zidaneadcanjo-jpg <zidaneadcanjo@gmail.com>
2025-11-10 08:29:59 +0100

    Update video-da-semana.js

--- video-da-semana.js
+++ video-da-semana.js
@@ -1,36 +1,10 @@
-async function carregarVideoDaSemana() {
-    const res = await fetch("videos.json");
-    const videos = await res.json();
-
-    let melhor = null;
-    let maiorScore = 0;
-
-    videos.forEach(v => {
-        const score = v.likes + v.comentarios + v.partilhas;
-        if (score > maiorScore) {
-            maiorScore = score;
-            melhor = v;
-        }
-    });
-
-    if (!melhor) return;
-
-    document.getElementById("video-semana-box").innerHTML = `
-        <div class="video-semana-card">
-            <iframe 
-                src="https://www.facebook.com/plugins/video.php?href=${melhor.link}" 
-                frameborder="0" allowfullscreen></iframe>
-
-            <h3>${melhor.titulo}</h3>
-            <p>${melhor.descricao}</p>
-
-            <div class="info-semana">
-                <span>👍 ${melhor.likes.toLocaleString()}</span>
-                <span>💬 ${melhor.comentarios.toLocaleString()}</span>
-                <span>↗️ ${melhor.partilhas.toLocaleString()}</span>
-            </div>
-        </div>
-    `;
-}
-
-carregarVideoDaSemana();
+document.getElementById("video-semana-box").innerHTML = `
+    <iframe
+        src="https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/share/r/1W9Sunt19E/&show_text=false&width=700"
+        style="width:100%;max-width:550px;height:320px;border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+    </iframe>
+`;
