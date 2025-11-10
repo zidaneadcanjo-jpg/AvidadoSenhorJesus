3136a3a8d7ddee8a88cb6315772059b8da5f45b5
zidaneadcanjo-jpg <zidaneadcanjo@gmail.com>
2025-11-10 09:09:02 +0100

    Update video-da-semana.js

--- video-da-semana.js
+++ video-da-semana.js
@@ -1,10 +1,12 @@
+const link = encodeURIComponent("https://www.facebook.com/share/r/1W9Sunt19E/");
+
 document.getElementById("video-semana-box").innerHTML = `
-    <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/share/r/1W9Sunt19E/&show_text=false&width=700"
-        style="width:100%;max-width:550px;height:320px;border:none;overflow:hidden"
-        scrolling="no"
-        frameborder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
-    </iframe>
+<iframe
+    src="https://www.facebook.com/plugins/video.php?href=${link}&show_text=false&width=500"
+    style="width:100%;height:320px;border:none;overflow:hidden;background:#000"
+    scrolling="no"
+    frameborder="0"
+    allowfullscreen="true"
+    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+</iframe>
 `;
